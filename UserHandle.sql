35232a81d09d69d294d5809bc82f768f9a43cece
pffelipe <pffelipe@PFFELIPE-LAP.oradev.oraclecorp.com>
2021-03-24 22:16:05 -0300

    Novas alteracoes

--- UserHandle.sql
+++ UserHandle.sql
@@ -1,14 +1,12 @@
 
 CREATE DATABASE `datauser` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-
 CREATE TABLE `user` (
   `iduser` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
   `avatar` blob,
   PRIMARY KEY (`iduser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 
 CREATE TABLE `usermovieviewed` (
   `idusermovieviewed` int(11) NOT NULL AUTO_INCREMENT,
@@ -18,7 +16,6 @@ CREATE TABLE `usermovieviewed` (
   UNIQUE KEY `IDX_VIEWED` (`iduser`,`idmovie`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
 CREATE TABLE `usermoviewatch` (
   `idusermoviewatch` int(11) NOT NULL AUTO_INCREMENT,
   `iduser` int(11) NOT NULL,
@@ -26,4 +23,3 @@ CREATE TABLE `usermoviewatch` (
   PRIMARY KEY (`idusermoviewatch`),
   UNIQUE KEY `IDX_WATCH` (`iduser`,`idmovie`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
